7ee779cc1573f945ecfda89686dd2e4fd3e952be
Milton Llera <lleramontero@gmail.com>
2019-06-15 16:05:33 +0100

    Updated handwritten number generation.

--- handwritten_numbers.py
+++ handwritten_numbers.py
@@ -1,34 +1,33 @@
 import numpy as np
 from itertools import product, cycle
-import torch
 
-from torch.utils.data import DataLoader, Dataset
+import torch
 import torchvision.transforms as trans
 import torchvision.datasets as dataset
 
 
-def group_img_by_digit(mnist):
-    ixc_pair = [(mnist[i][1], i) for i in range(len(mnist))]
-    ixc_pair  = sorted(ixc_pair, key=lambda x: x[0])
+# def group_img_by_digit(mnist):
+#     ixc_pair = [(mnist[i][1], i) for i in range(len(mnist))]
+#     ixc_pair  = sorted(ixc_pair, key=lambda x: x[0])
 
-    current = ixc_pair[0][0]
-    groups = {current: []}
+#     current = ixc_pair[0][0]
+#     groups = {current: []}
 
-    for num, idx in ixc_pair:
-        if num == current:
-            groups[current].append(idx)
-        else:
-            current = num
-            groups[current] = [idx]
+#     for num, idx in ixc_pair:
+#         if num == current:
+#             groups[current].append(idx)
+#         else:
+#             current = num
+#             groups[current] = [idx]
 
-    return groups
+#     return groups
 
 
 def trim_leading_zeros(img_shape, images, targets):
     empty_image = torch.zeros_like(images[0, 0])
 
-    for i in range(images.shape[0]-1):
-        for j in range(images.shape[1]):
+    for i in range(images.shape[0]):
+        for j in range(images.shape[1] - 1):
             if targets[i * images.shape[1] + j] == 0:
                 images[i, j] = empty_image
             else:
@@ -102,7 +101,7 @@ class HandwrittenNumberGenerator:
         inputs = inputs.view(self.batch_size, -1)
 
         if self.supervised:
-            targets = process_labels(targets, batch_shape)
+            targets = process_labels(labels, batch_shape)
         else:
             targets = inputs
 
@@ -173,7 +172,7 @@ def load_handwritten_number_data(
         supervised=supervised,
         dataset_size=dataset_size,
         batch_size=batch_size,
-        leadning_zeros=False,
+        leading_zeros=False,
         infinite_gen=True,
         random_state=random_state.randint(2**32-1) if not infinite_gen else random_state
     )
@@ -185,25 +184,23 @@ def load_handwritten_number_data(
         supervised=supervised,
         dataset_size=dataset_size,
         batch_size=batch_size,
-        leadning_zeros=False,
+        leading_zeros=False,
         infinite_gen=True,
         random_state=random_state.randint(2**32-1) if not infinite_gen else random_state
     )
 
-    if infinite_gen:
-        validation_set = training_set
-    else:
-        validation_set = HandwrittenNumberGenerator(
-            mnist=train_raw,
-            ndigits=ndigits,
-            digit_shape=digit_shape,
-            supervised=supervised,
-            dataset_size=dataset_size,
-            batch_size=batch_size,
-            leadning_zeros=False,
-            infinite_gen=True,
-            rng=random_state.randint(2**32-1) if not infinite_gen else random_state
-        )
+    validation_set = HandwrittenNumberGenerator(
+        mnist=train_raw,
+        ndigits=ndigits,
+        digit_shape=digit_shape,
+        supervised=supervised,
+        # validation set size as a fraction of training set size
+        dataset_size=int(0.1 * dataset_size),
+        batch_size=batch_size,
+        leadning_zeros=False,
+        infinite_gen=True,
+        rng=random_state.randint(2**32-1) if not infinite_gen else random_state
+    )
 
     return training_set, test_set, validation_set
 
@@ -224,8 +221,7 @@ def load_handwritten_sequence(
 
     train_raw, test_raw = load_raw(mnist_path, digit_shape, download)
 
-    training_gen = []
-    test_gen = []
+    training_gen, validation_gen, test_gen = [], [], []
 
     if random_state is None or isinstance(random_state, int):
         random_state = np.random.RandomState(random_state)
@@ -258,26 +254,23 @@ def load_handwritten_sequence(
             random_state=random_state if infinite_gen else random_state.randint(2**32-1)
         ))
 
+    for i in range(seqlen):
+        data = train_raw if train_filters is None else filter(train_filters[i], train_raw)
+        validation_gen.append(HandwrittenNumberGenerator(
+            mnist=data,
+            ndigits=ndigits,
+            digit_shape=digit_shape,
+            supervised=supervised,
+            # validation set size as a fraction of training set size
+            dataset_size=int(0.1 * size),
+            batch_size=batch_size,
+            leading_zeros=leading_zeros,
+            infinite_gen=infinite_gen,
+            random_state=random_state if infinite_gen else random_state.randint(2**32-1)
+        ))
+
     training_set = SequenceGenerator(training_gen)
     test_set = SequenceGenerator(test_gen)
-
-    if not infinite_gen:
-        validation_gen = []
-        for i in range(seqlen):
-            data = train_raw if train_filters is None else filter(train_filters[i], train_raw)
-            validation_gen.append(HandwrittenNumberGenerator(
-                mnist=data,
-                ndigits=ndigits,
-                digit_shape=digit_shape,
-                supervised=supervised,
-                dataset_size=dataset_size,
-                batch_size=batch_size,
-                leading_zeros=leading_zeros,
-                infinite_gen=infinite_gen,
-                random_state=random_state.randint(2**32-1)
-            ))
-        validation_set = SequenceGenerator(validation_gen)
-    else:
-        validation_set = training_set
+    validation_set = SequenceGenerator(validation_gen)
 
     return training_set, validation_set, test_set
